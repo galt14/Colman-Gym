48e2c9d31568156444f7b429c37975bf39ea450c
Guy-Rozanes <guyrozanes18@gmail.com>
2021-11-03 15:42:18 +0200

    add more css features and animations

--- ColmanGym/ColmanGym/Areas/Identity/Pages/Register.cshtml.cs
+++ ColmanGym/ColmanGym/Areas/Identity/Pages/Register.cshtml.cs
@@ -131,7 +131,7 @@ namespace ColmanGym.Areas.Identity.Pages
                         protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        $"Please confirm your user account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (user.IsTrainer)
                     {
